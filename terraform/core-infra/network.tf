86989e1394687249c9462115c93d773fafbb867d
Sooter Saalu <53571625+Soot3@users.noreply.github.com>
2025-07-12 20:13:11 +0100

    Update network.tf

--- terraform/core-infra/network.tf
+++ terraform/core-infra/network.tf
@@ -29,35 +29,6 @@ module "network-dev" {
   }
 }
 
-module "network-prod" {
-  source = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.4"
-
-  name                        = "prod"
-  cidr                        = var.vpc_cidr
-  azs                         = data.aws_availability_zones.available.names
-  public_subnets              = var.public_subnets
-  private_subnets             = var.private_subnets
-  enable_dns_hostnames        = true
-  enable_ipv6                 = false
-  public_subnet_enable_dns64  = false
-  private_subnet_enable_dns64 = false
-
-  enable_nat_gateway = true
-  single_nat_gateway = false
-
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = "1", 
-    "tier" = "public"
-  }
-  
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = "1", 
-    "tier" = "private"
-  }
-}
-
 resource "plural_service_context" "dev-vpc" {
     name = "plrl/vpc/dev"
 
@@ -69,21 +40,6 @@ resource "plural_service_context" "dev-vpc" {
         vpc_cidr        = var.vpc_cidr
         
         ingress_dns_zone = "dev.drafttests.click"
-        
-    })
-}
-
-resource "plural_service_context" "prod-vpc" {
-    name = "plrl/vpc/prod"
-
-    configuration = jsonencode({
-        vpc_id          = module.network-prod.vpc_id
-        subnet_ids      = concat(module.network-prod.public_subnets, module.network-prod.private_subnets)
-        private_subnets = module.network-prod.private_subnets
-        public_subnets  = module.network-prod.public_subnets
-        vpc_cidr        = var.vpc_cidr
-        
-        ingress_dns_zone = "drafttests.click"
         
     })
 }
